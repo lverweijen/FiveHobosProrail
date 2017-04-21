97d6b85f65291abaef38d77ce4179da2fc90254b
lverweijen <lverweijen>
2017-04-21 10:16:29 +0200

    Commit stuff i shouldn't commit, because it's sort of binary, you know and this commit message is longer than 72 chars, which is bad.

---
+++ DetectText/build/CMakeFiles/DetectText.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DetectText.dir/FeaturesMain.cpp.o"
+  "CMakeFiles/DetectText.dir/TextDetection.cpp.o"
+  "DetectText.pdb"
+  "DetectText"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DetectText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
